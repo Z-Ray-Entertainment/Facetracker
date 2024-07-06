bf3d4f3edd19f5d441051e44c0cb489d19af5104
Vortex Acherontic <vortex@z-ray.de>
2024-07-06 10:39:21 +0200

    #3 Fix launch as flatpak app

--- facetracker/__init__.py
+++ facetracker/__init__.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
 import sys
 
+from os import environ
+
 pkgdatadir = '@pkgdatadir@'
-sys.path.insert(1, pkgdatadir)
+if environ.get("FLATPAK_ID") is not None:
+    sys.path.insert(1, pkgdatadir)
 
 from facetracker.const import APP_ID
 from facetracker.main_frame import OpenSeeFaceFacetrackingWrapper
 
 
 def run():
+    OpenSeeFaceFacetrackingWrapper(application_id=APP_ID).run(sys.argv)
+
+
+if __name__ == '__main__':
     OpenSeeFaceFacetrackingWrapper(application_id=APP_ID).run(sys.argv)
